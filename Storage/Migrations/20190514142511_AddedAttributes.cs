8310998800c24f533db14f2e0e0ac36a7d2f591a
TB <tb@example.com>
2019-05-14 16:26:04 +0200

    Add project files.

---
+++ Storage/Migrations/20190514142511_AddedAttributes.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Storage.Migrations
+{
+    public partial class AddedAttributes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Category",
+                table: "Product",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Category",
+                table: "Product",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
